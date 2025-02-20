40716096a6a1505c57c87410c6cb2cc8c1968d0f
YB L <87692810+dldudqlsdlqslek@users.noreply.github.com>
2025-02-20 17:53:40 +0900

    add ai

--- Bot.java
+++ Bot.java
@@ -7,11 +7,37 @@ public class Bot {
     public static void main(String[] args){
         String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
         String message = System.getenv("SLACK_WEBHOOK_MSG");
+
+        String aikey = System.getenv("GEMINI_KEY");
+        String aiurl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="+ aikey;
+
+
+
+        HttpClient aiclient = HttpClient.newHttpClient();
+        HttpRequest airequest = HttpRequest.newBuilder()
+            .uri(URI.create(aiurl))
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{\"contents\": [{\"parts\":[{\"text\": \"웃는 표정 이모티콘 하나만 줘\"}]}]}")) 
+            .build();
+
+        try{
+            HttpResponse<String> airesponse = aiclient.send(
+                airequest, HttpResponse.BodyHandlers.ofString()
+            );
+            System.out.println("요청 코드 : " + airesponse.statusCode());
+            System.out.println("응답 결과 : " + airesponse.body());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\" + " + message + "\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"" + airesponse.body() + "\"}")) 
             .build();
 
         try{
