8ddbf3f25197f20dc672b8569be3dd5e3a105035
YB L <87692810+dldudqlsdlqslek@users.noreply.github.com>
2025-02-20 17:17:39 +0900

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -10,7 +10,7 @@ public class Bot {
         HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
+            .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"('◡')\"}")) 
             .build();
 
         try{
